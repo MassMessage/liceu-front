7d1495602bb7a5c393dde3a0d8a8d288776ac3b9
alexandrexyz <costaalexandre45@gmail.com>
2023-12-23 02:56:07 -0300

    Add table section

--- app/components/SectionProfile.tsx
+++ app/components/SectionProfile.tsx
@@ -2,11 +2,12 @@ import { FC, ReactNode } from "react";
 
 interface ISectionProfile {
     children: ReactNode | string;
+    borderNone?: boolean
 }
 
-export const SectionProfile: FC<ISectionProfile> = ({ children }) => {
+export const SectionProfile: FC<ISectionProfile> = ({ children, borderNone }) => {
     return (
-        <section className='p-12 flex items-center flex-col border-b w-full border-[#A7A7A7]'>
+        <section className={`p-12 flex items-center flex-col ${!borderNone ? 'border-b' : null} w-full border-[#A7A7A7]`}>
             {children}
         </section>
     )
